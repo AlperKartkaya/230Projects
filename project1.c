3c4cb33bf4d4f77ae7e578a4f10ef7a7d00eb547
AlperKartkaya <alperkartkaya@gmail.com>
2025-02-27 17:39:35 +0300

    Add age input and output to project1.c

--- project1.c
+++ project1.c
@@ -1,8 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
     printf("Hello World\n");
     printf("Hello World2\n");
+    int age;
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    printf("Your age is: %d\n", age);
     return 0;
 }
