d375419045ccc8ddcd99bfe417f7a33496d9aa62
AlperKartkaya <alperkartkaya@gmail.com>
2025-03-10 08:32:58 +0300

    simplification is done

--- project1.c
+++ project1.c
@@ -3,10 +3,5 @@
 int main()
 {
     printf("Hello World\n");
-    printf("Hello World2\n");
-    int age;
-    printf("Enter your age: ");
-    scanf("%d", &age);
-    printf("Your age is: %d\n", age);
-    return 0;
+    
 }
